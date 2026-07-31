6fb224c42404554075e25da99692b5116d7e072b
Claudio <claudio.bonini@retesi.it>
2026-07-31 11:18:19 +0200

    Nuovo portale Self Defense Sondrio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.difesapersonalesondrio.it',
-  integrations: [sitemap()],
   output: 'static'
 });
