d2308d875d8b1da8cfeb6ae4fedf416b9d5e6571
Claudio <claudio.bonini@retesi.it>
2026-08-02 15:50:22 +0200

    Aggiunge staff, mappe e contenuti condivisi

---
+++ src/data/navigation.ts
@@ -0,0 +1,9 @@
+export const primaryNavigation = [
+  { label: 'Home', href: '/' },
+  { label: 'Staff', href: '/staff' },
+  { label: 'Corsi', href: '/corsi' },
+  { label: 'Metodo', href: '/metodo' },
+  { label: 'Associazione', href: '/associazione' },
+  { label: 'FAQ', href: '/#faq' },
+  { label: 'Contatti', href: '/contatti' }
+] as const;
