3b478fe1cc9514394bf8796387a49cdbad6f18d6
Claudio <claudio.bonini@retesi.it>
2026-08-04 15:21:49 +0200

    Rimuove file duplicati e asset inutilizzati

--- astro.config 2.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-
-export default defineConfig({
-  site: 'https://www.selfdefensesondrio.it',
-  integrations: [sitemap()],
-  output: 'static'
-});
