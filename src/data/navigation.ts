4af1291eac709db9376e42af096f4d6ac4c7dbac
Claudio <claudio.bonini@retesi.it>
2026-08-06 21:34:04 +0200

    Rende lineare la navigazione del sito

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -1,7 +1,9 @@
 export const primaryNavigation = [
   { label: 'Home', href: '/' },
-  { label: 'Corsi', href: '/corsi', children: [{ label: 'Metodo', href: '/metodo' }] },
-  { label: 'Associazione', href: '/associazione', children: [{ label: 'Staff', href: '/staff' }] },
+  { label: 'Corsi', href: '/corsi' },
+  { label: 'Metodo', href: '/metodo' },
+  { label: 'Associazione', href: '/associazione' },
+  { label: 'Staff', href: '/staff' },
   { label: 'News', href: '/news' },
   { label: 'FAQ', href: '/faq' },
   { label: 'Contatti', href: '/contatti' }
