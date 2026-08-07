86c6d549bbfa7f86919a84327bbb7842d204e0bb
Claudio <claudio.bonini@retesi.it>
2026-08-07 22:17:03 +0200

    Ottimizza CSS e immagini del sito

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://selfdefensesondrio.it',
   output: 'static',
+  build: {
+    inlineStylesheets: 'always'
+  },
   integrations: [
     sitemap()
   ]
